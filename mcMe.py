767b38860d5dc95f8861ad073c344a716c14c727
dmarsola <marsola.douglas@cegep-heritage.qc.ca>
2019-03-13 15:19:19 -0400

    Update mcMe.py
    New print functionality.

--- mcMe.py
+++ mcMe.py
@@ -2,5 +2,8 @@ def mc_show():
     print("Marissa Cleroux 03/13/2019 - Programming IV 420B43")
     
 def print_nums():
-    for i in range(1, 10):
+    for i in range(1, 11):
         print(i)
+
+def dm_new():
+    print("Hello Marissa")
