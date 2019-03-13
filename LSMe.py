c29e70118c10178bef50ffde80baa3d7af195ccb
Lianne Spurdle <spurdle.lianne@cegep-heritage.qc.ca>
2019-03-13 15:22:47 -0400

    Update LSMe.py

--- LSMe.py
+++ LSMe.py
@@ -3,3 +3,8 @@ import datetime
 def ls_show():
     ct = datetime.datetime.now()
     print(f"Lianne {ct.strfttime("%Y/%m/%d")} 420-B43 Programming IV")
+
+
+def print_nums():
+    for n in range(1, 11):
+        print(f"{n}")
