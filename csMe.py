b88dbdc257bcd3a889f31ed70bd534331ba51bfb
manuelifeno <manu.n.v@hotmail.es>
2019-03-13 15:47:19 -0400

    My second commit

--- csMe.py
+++ csMe.py
@@ -2,4 +2,7 @@ def cs_show():
     print('Chelsea')
     print('Programming IV - 420-B43')
     print('March 13, 2019')
-   
+
+def mn_new():
+    print("Manuel Nieves")
+    print("Hello Chelsea!")
